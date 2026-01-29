90a95129a9e20a8cad8b8bd6ef33508f59ac1647
javiergs <3814755+javiergs@users.noreply.github.com>
2026-01-29 02:58:58 -0800

    Working with threads

---
+++ 07-threads-gui/Blackboard.java
@@ -0,0 +1,28 @@
+package threading;
+
+import java.beans.PropertyChangeSupport;
+
+public class Blackboard extends PropertyChangeSupport {
+	
+	private static final Blackboard instance = new Blackboard();
+	private int[] numbers = new int[5];
+	
+	private Blackboard() {
+		super(new Object());
+	}
+	
+	public static Blackboard getInstance() {
+		return instance;
+	}
+	
+	public void setNumber(int index, int value) {
+		numbers[index] = value;
+		firePropertyChange("numbers", null, value);
+	}
+	
+	public int getNumber(int index) {
+		return numbers[index];
+	}
+	
+}
+
---
+++ 07-threads-gui/Main.java
@@ -0,0 +1,28 @@
+package threading;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+	
+	public Main() {
+		View view = new View();
+		add(view);
+		Blackboard.getInstance().addPropertyChangeListener(view);
+	}
+	
+	public static void main(String[] args) {
+		Main frame = new Main();
+		frame.setTitle("Threads + GUI");
+		frame.setSize(400, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		for (int i = 0; i < 5; i++) {
+			Thread generator = new Thread(new Generator(i, i*100, 100 + i * 1000));
+			generator.start();
+		}
+	}
+	
+}
+
